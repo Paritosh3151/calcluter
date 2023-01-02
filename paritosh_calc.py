9766dc2190b4b4ad7b270969766e3718e3eca2d9
paritosh3151 <paritosh.kumar3151@gmail.com>
2023-01-02 18:23:37 +0530

    assignment

---
+++ paritosh_calc.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+a=int(input("enter first number"))
+
+
+# In[5]:
+
+
+b=int(input("enter second number"))
+
+
+# In[6]:
+
+
+c=int(input("enter third number"))
+
+
+# In[7]:
+
+
+d=float(input("enter fourth number"))
+
+
+# In[8]:
+
+
+import math
+
+
+# In[9]:
+
+
+print("sum of the number is:",a+b)
+
+
+# In[10]:
+
+
+print("sub of the number is:",a-b)
+
+
+# In[11]:
+
+
+print("mul of the number is:",a*b)
+
+
+# In[12]:
+
+
+print("div of the number is:",a/b)
+
+
+# In[13]:
+
+
+print("reminder of the number is:",b/c)
+
+
+# In[14]:
+
+
+print("avg of the number is:",(a+b)/2)
+
+
+# In[15]:
+
+
+print("round the number upto 2 dicimal place is:",round(d,2))
+
+
+# In[16]:
+
+
+print("ceil the number is:",math.ceil(d))
+
+
+# In[17]:
+
+
+print("floor the number is:",math.floor(d))
+
+
+# In[ ]:
+
+
+
+
